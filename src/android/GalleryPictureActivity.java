d06adc593500485bfd23f913c62f21cf6d734824
Abraham Cedeno <abraham.cedeno@vivacetek.com>
2015-09-16 15:41:39 -0430

    Forgot to import drawable class

--- src/android/GalleryPictureActivity.java
+++ src/android/GalleryPictureActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
